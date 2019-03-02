488da1eaca47ea90b630670e51c3a2796b45d50f
Benitp <b.kriel34@gmail.com>
2019-03-02 12:37:20 +0200

    Adding Map

--- src/main/java/ac/za/cput/App.java
+++ src/main/java/ac/za/cput/App.java
@@ -16,17 +16,32 @@ public class App
         Person personObject1= new Person("Connie", 1200);
         Person personObject2= new Person("Robyn", 200);
 
+        //List
         List<Person> person = new ArrayList<>();
         person.add(new Person("Zuma", 15000));
         person.add(new Person("Julius", 5000));
         person.add(new Person("Cyril", 600000));
 
+        System.out.println("--------------------------------------");
+        //map
+        Map <Person, Integer> map = new LinkedHashMap<Person, Integer>();
+        map.put(personObject,1);
+        map.put(personObject1,2);
+        map.put(personObject2,3);
+
+        for (Person key: map.keySet())
+        {
+            System.out.println(key + ": " + map.get(key));
+
+        }
+        System.out.println("--------------------------------------");
+        //set
         Set<Person> set = new LinkedHashSet<>();
         set.add(personObject);
         set.add(personObject1);
         set.add(personObject2);
 
         System.out.println(set);
-
+        System.out.println("--------------------------------------");
     }
 }
