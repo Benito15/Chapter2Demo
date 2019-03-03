414fcc7de582a89a9f28ddd758d62a752958a9c8
Benitp <b.kriel34@gmail.com>
2019-03-03 06:51:04 +0200

    TDD springFrameWork

---
+++ src/test/java/ac/za/cput/SuperHeroeTest.java
@@ -0,0 +1,26 @@
+package ac.za.cput;
+
+public class SuperHeroeTest extends AbstractTestCollection{
+
+    private SuperHero superHero;
+    private String name = "Spiderman";
+    private int amount = 500000;
+
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void test() {
+
+    }
+
+    @Override
+    public void end() {
+
+    }
+
+
+}
