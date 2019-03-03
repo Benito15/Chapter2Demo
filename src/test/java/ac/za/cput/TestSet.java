7b3c8628b37a0e87843902023746683d5e54aab1
Benitp <b.kriel34@gmail.com>
2019-03-03 05:56:22 +0200

    set TDD solution unit testing

---
+++ src/test/java/ac/za/cput/TestSet.java
@@ -0,0 +1,38 @@
+package ac.za.cput;
+
+import org.junit.Assert;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestSet extends AbstractTestCollection{
+
+    Set<Integer> set;
+
+    public void initialize()
+    {
+
+        set = new HashSet<>();
+        set.add(15);
+        set.add(25);
+        set.add(35);
+
+    }
+
+    @Override
+    public void test() {
+        for (Integer i: set)
+        {
+            Assert.assertEquals(i + "total of i", 75, i + i);
+
+        }
+
+    }
+
+
+    @Override
+    public void end() {
+        set = null;
+
+    }
+}
